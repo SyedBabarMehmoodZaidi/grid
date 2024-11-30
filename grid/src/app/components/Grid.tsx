0543cd32b9c95fac4486d63198ef2f7fb55bff93
Babar Mehmood <babarzaidi1122@gmail.com>
2024-11-30 15:20:35 +0500

    commit

---
+++ grid/src/app/components/Grid.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const grid = () => {
+  return (
+    <div className='grid grid-cols-4 grid-rows-5 h-screen gap-2 p-2'>
+    <div className='bg-pink-950 text-white flex justify-center items-center col-span-5'>Header</div>
+    <div className='bg-yellow-500 text-white flex justify-center items-center row-span-2'>Side Bar</div>
+    <div className='bg-red-700 text-white flex justify-center items-center col-span-4'>Content 1</div>
+    <div className='bg-red-700 text-white flex justify-center items-center col-span-1'>Content 2</div>
+    <div className='bg-red-700 text-white flex justify-center items-center col-span-1'>Content 3</div>
+    <div className='bg-green-500 text-white flex justify-center items-center col-span-5'>Footer</div>  
+    
+    </div>
+  )
+}
+
+export default grid
---
+++ grid/src/app/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Grid from './Grid';
+const home = () => {
+  return (
+    <div>
+    <Grid />
+    </div>
+  )
+}
+
+export default home;
